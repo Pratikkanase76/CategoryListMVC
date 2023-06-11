dbdf23b5c5d968fd75d64c3bed2237245e5d698d
Pratikkanase76 <kanase.pratik007@gmail.com>
2023-06-11 18:56:20 +0530

    Add project files.

---
+++ CategoryListMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CategoryListMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
